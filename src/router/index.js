6024c2ac87d40e16eb095c0ec4df987833f63429
2407592711@qq.com <fushoudong>
2023-10-22 10:55:44 +0800

    feat:

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/views/login/index'
+import Register from '@/views/register/index'
 
 Vue.use(Router)
 
@@ -8,8 +9,21 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Default',
+      redirect: '/login',
+      meta: {
+        name: '首页'
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
---
+++ src/views/login/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="login_main">
+        这是登录页面
+        <div style="margin-left: 15px; width: 30%">
+            <el-input v-model="search_text" placeholder="请输入要搜索的内容">
+                <template slot="append">
+                    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+                </template>
+            </el-input>
+        </div>
+    </div>
+</template>
+<script>
+import ELSlPanel from 'element-ui/packages/color-picker/src/components/sv-panel'
+export default {
+    name: 'Login',
+    components: {
+        ELSlPanel
+    },
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    methods: {
+        submitForLogin() {
+
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
